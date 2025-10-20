1a198450cf82905894b7ca0c54f56a2264233d59
zenoura00 <zenoura28@gmail.com>
2025-10-21 01:22:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/light-therapy-facial-x7-store/',
+  base: '/mask/', // 👈 اسم مستودع GitHub بالضبط
   plugins: [react()],
 })
